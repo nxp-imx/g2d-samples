cf559737a73ee54aa3a70d4707b41d3743130617
Guangliu Ding <guangliu.ding@nxp.com>
2024-05-17 13:59:08 +0800

    MGS-7716-1 fix YUYV dst fwrite error in g2d_yuv_test
    Corret size of YUYV buffer to dstStride * dstHeight * 2.
    
    Signed-off-by: Guangliu Ding <guangliu.ding@nxp.com>

--- yuv_test/g2d_yuv.c
+++ yuv_test/g2d_yuv.c
@@ -367,7 +367,7 @@ int main(int argc, char **argv) {
   printf("\nLinear conversion done %f ms  \n", t / TEST_LOOP);
 
   if (G2D_YUYV == dstFmt) {
-    fwrite(dstYBuf->buf_vaddr, 1, dstStride * dstHeight, fpout);
+    fwrite(dstYBuf->buf_vaddr, 1, dstStride * dstHeight * 2, fpout);
   } else if (G2D_NV12 == dstFmt) {
     fwrite(dstYBuf->buf_vaddr, 1, dstStride * dstHeight, fpout);
     fwrite(dstUBuf->buf_vaddr, 1, dstStride * dstHeight / 2, fpout);
